ac2771084860c54c2fc5d2f4f3257f63fdccf6d8
unknown <parkbyeonghak96@gmail.com>
2023-03-29 16:27:30 +0900

    Customer.js 파일 생성했습니다. map 기능을 사용해봤습니다.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
 
-const customer = {        //props를 만들어서 값을 작성
-  'name' : '홍길동',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-}
+const customers = [
+  {        //props를 만들어서 값을 작성
+    'id' : 1,
+    'image' : 'https://placeimg.com/128/128/any/1',
+    'name' : '홍길동',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {        //props를 만들어서 값을 작성
+    'id' : 2,
+    'image' : 'https://placeimg.com/128/128/any/2',
+    'name' : '이순신',
+    'birthday' : '920102',
+    'gender' : '남자',
+    'job' : '직장인'
+  },
+  {        //props를 만들어서 값을 작성
+    'id' : 3,
+    'image' : 'https://placeimg.com/128/128/any/3',
+    'name' : '유관순',
+    'birthday' : '861220',
+    'gender' : '여자',
+    'job' : '프로그래머'
+  }
+]
 class App extends Component {
   render() {
-    return (
-        <Customer         //전달 받은 props를 이용하여 구현
-          name={customer.name}
-          birthday={customer.birthday}
-          gender={customer.gender}
-          job={customer.job}
-        />
+    return (            //JSX는 반드시 하나의 태그로만 이루어짐(아래처럼 div로 모두 감싸면 됨)
+      <div>
+        {
+          customers.map(c => {    //map을 사용해서 여러 사용자의 정보를 간결하게 표현함.
+            return (              //map을 이용하려면 구분할 수 있는 key 가 있어야함.
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,10 +1,33 @@
 import React from "react";
 
+//고객정보를 총 두개의 컴포넌트로 구성함
 class Customer extends React.Component {//React컴포넌트가 있으니 this키워드를 사용하여 출력
     render() {
         return(
             <div>
-                <h2>{this.props.name}</h2>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+            </div>
+        )
+    }
+}
+
+//아래의 두 컴포넌트로 데이터를 나눠서 저장
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
                 <p>{this.props.birthday}</p>
                 <p>{this.props.gender}</p>
                 <p>{this.props.job}</p>
