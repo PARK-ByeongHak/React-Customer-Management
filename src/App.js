2677bc6b15bc4ec5414561d191680bd78034940e
unknown <parkbyeonghak96@gmail.com>
2023-03-29 17:52:48 +0900

    Mui를 사용하여 스타일을 꾸며봤습니다. 영상이 이전 버전이라 style은 구현에 실패했습니다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+
+
 
 const customers = [
   {        //props를 만들어서 값을 작성
     'id' : 1,
-    'image' : 'https://placeimg.com/128/128/any/1',
+    'image' : 'https://placeimg.com/64/64/any/1',
     'name' : '홍길동',
     'birthday' : '961222',
     'gender' : '남자',
@@ -13,7 +21,7 @@ const customers = [
   },
   {        //props를 만들어서 값을 작성
     'id' : 2,
-    'image' : 'https://placeimg.com/128/128/any/2',
+    'image' : 'https://placeimg.com/64/64/any/2',
     'name' : '이순신',
     'birthday' : '920102',
     'gender' : '남자',
@@ -21,7 +29,7 @@ const customers = [
   },
   {        //props를 만들어서 값을 작성
     'id' : 3,
-    'image' : 'https://placeimg.com/128/128/any/3',
+    'image' : 'https://placeimg.com/64/64/any/3',
     'name' : '유관순',
     'birthday' : '861220',
     'gender' : '여자',
@@ -30,24 +38,30 @@ const customers = [
 ]
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (            //JSX는 반드시 하나의 태그로만 이루어짐(아래처럼 div로 모두 감싸면 됨)
-      <div>
-        {
-          customers.map(c => {    //map을 사용해서 여러 사용자의 정보를 간결하게 표현함.
-            return (              //map을 이용하려면 구분할 수 있는 key 가 있어야함.
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            )
-          })
-        }
-      </div>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {    //map을 사용해서 여러 사용자의 정보를 간결하게 표현함.
+                return (              //map을 이용하려면 구분할 수 있는 key 가 있어야함.
+                  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,39 +1,22 @@
 import React from "react";
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 //고객정보를 총 두개의 컴포넌트로 구성함
 class Customer extends React.Component {//React컴포넌트가 있으니 this키워드를 사용하여 출력
     render() {
-        return(
-            <div>
-                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+        return (
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
 
-//아래의 두 컴포넌트로 데이터를 나눠서 저장
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 
 export default Customer;
