b7c14a5a59fb3974d8fe2970328d27f35cd22725
unknown <parkbyeonghak96@gmail.com>
2023-03-29 20:39:30 +0900

    Node.js Express 에서 고객 api를 구현했습니다. Proxy에서 문제가 있어 새로운 방법을 사용했습니다.

--- client/src/App.js
+++ client/src/App.js
@@ -9,36 +9,25 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 
 
-
-const customers = [
-  {        //props를 만들어서 값을 작성
-    'id' : 1,
-    'image' : 'https://placeimg.com/64/64/any/1',
-    'name' : '홍길동',
-    'birthday' : '961222',
-    'gender' : '남자',
-    'job' : '대학생'
-  },
-  {        //props를 만들어서 값을 작성
-    'id' : 2,
-    'image' : 'https://placeimg.com/64/64/any/2',
-    'name' : '이순신',
-    'birthday' : '920102',
-    'gender' : '남자',
-    'job' : '직장인'
-  },
-  {        //props를 만들어서 값을 작성
-    'id' : 3,
-    'image' : 'https://placeimg.com/64/64/any/3',
-    'name' : '유관순',
-    'birthday' : '861220',
-    'gender' : '여자',
-    'job' : '프로그래머'
-  }
-]
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const responce = await fetch('/api/customers');
+    const body = await responce.json();
+    return body;
+  }
+
   render() {
-    const { classes } = this.props;
     return (            //JSX는 반드시 하나의 태그로만 이루어짐(아래처럼 div로 모두 감싸면 됨)
       <Paper>
         <Table>
@@ -53,12 +42,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {    //map을 사용해서 여러 사용자의 정보를 간결하게 표현함.
-                return (              //map을 이용하려면 구분할 수 있는 key 가 있어야함.
-                  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-                )
-              })
-            }
+            {this.state.customers ? this.state.customers.map(c => {    //map을 사용해서 여러 사용자의 정보를 간결하게 표현함.
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> );
+              }) : "" }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const port  =process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id' : 1,
+          'image' : 'https://placeimg.com/64/64/any/1',
+          'name' : '홍길동',
+          'birthday' : '961222',
+          'gender' : '남자',
+          'job' : '대학생'
+        },
+        {
+          'id' : 2,
+          'image' : 'https://placeimg.com/64/64/any/2',
+          'name' : '이순신',
+          'birthday' : '920102',
+          'gender' : '남자',
+          'job' : '직장인'
+        },
+        {
+          'id' : 3,
+          'image' : 'https://placeimg.com/64/64/any/3',
+          'name' : '유관순',
+          'birthday' : '861220',
+          'gender' : '여자',
+          'job' : '프로그래머'
+        }
+      ])
+})
 
-app.listen(port, () => console.log(`Listening on poat ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
